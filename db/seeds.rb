10d474355c33e15d3dce76965c26ddf257815dca
Clare Glinka <glinka.cb@gmail.com>
2013-10-11 17:22:31 -0500

    add database seed file and schema.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+grilled_cheese = MenuItem.create({
+  name: "Grilled Cheese",
+  price: 5,
+  })
+
+turkey_sandwich = MenuItem.create({
+  name: "Turkey Sandwich",
+  price: 5,
+  })
+
+cheese_pizza = MenuItem.create({
+  name: "Cheese Pizza",
+  price: 5
+  })
+
+bread = Ingredient.create(name: "bread")
+cheese = Ingredient.create(name: "cheese")
+turkey = Ingredient.create(name: "turkey")
+mustard = Ingredient.create(name: "mustard")
+lettuce = Ingredient.create(name: "lettuce")
+mayo = Ingredient.create(name: "mayonaise")
+
+
+grilled_cheese.recipes.create(ingredient: cheese)
+grilled_cheese.recipes.create(ingredient: bread)
+
+turkey_sandwich.recipes.create(ingredient: bread)
+turkey_sandwich.recipes.create(ingredient: turkey)
+turkey_sandwich.recipes.create(ingredient: mustard)
+turkey_sandwich.recipes.create(ingredient: lettuce)
+turkey_sandwich.recipes.create(ingredient: mayo)
+
+cheese_pizza.recipes.create(ingredient: bread)
+cheese_pizza.recipes.create(ingredient: cheese)
